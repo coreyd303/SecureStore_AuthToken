acec140d8ae6883722d2cf0c7c92e0ffda1dd25f
Corey Davis <corey@pond5.com>
2019-04-02 18:24:13 -0600

    Initial Commit

---
+++ TokenStore/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  TokenStore
+//
+//  Created by Corey Davis on 4/2/19.
+//  Copyright © 2019 Pond5. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
